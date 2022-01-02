f617f127a77e3de59807faa611129a560552e08a
Mike Burger <mburger@broadinstitute.org>
2022-01-02 17:34:53 -0500

    Supplemental Figure 12

---
+++ src/gene_dep_profiles/LRT_scatter.R
@@ -0,0 +1,72 @@
+
+source("src/packages_paths.R")
+
+t1 <- fread(file.path("tables","Supplemental-Table-1.csv"))
+t1$entrez_id %<>% as.character(.)
+t1 %<>% subset(.,high_confidence)
+
+t2 <- fread(file.path("tables","Supplemental-Table-2.csv"))
+t2$entrez_id %<>% as.character(.)
+t2 %<>% subset(.,entrez_id %in% t1$entrez_id)
+
+normLRT <- dplyr::select(t2,symbol,
+                         CRISPR_LRT,RNAi_LRT)
+
+
+max_LRT <- max(c(normLRT$CRISPR_LRT,normLRT$RNAi_LRT),na.rm=T)
+min_LRT <- min(c(normLRT$CRISPR_LRT,normLRT$RNAi_LRT),na.rm=T)
+normLRT$X <- normLRT$CRISPR_LRT
+normLRT$X[normLRT$X < 0] <- 0
+normLRT$Y <- normLRT$RNAi_LRT
+normLRT$Y[normLRT$Y < 0] <- 0
+normLRT$X <- log2(normLRT$X + 100)
+normLRT$Y <- log2(normLRT$Y + 100)
+min_LRT <- log2(min_LRT+100)
+max_LRT <- log2(max_LRT+100)
+mypal <- c("#A44E78","#4186C4","#E49042")
+
+se_labels <- subset(normLRT,normLRT$CRISPR_LRT > 170 | normLRT$RNAi_LRT > 160)
+
+crispr_only <- subset(normLRT,normLRT$CRISPR_LRT > 100 & normLRT$RNAi_LRT < 100)
+crispr_only_count <- nrow(crispr_only)
+rnai_only <- subset(normLRT,normLRT$CRISPR_LRT < 100 & normLRT$RNAi_LRT > 100)
+rnai_only_count <- nrow(rnai_only)
+both <- subset(normLRT,normLRT$CRISPR_LRT > 100 & normLRT$RNAi_LRT > 100)
+both_count <- nrow(both)
+
+point_label_font = 6
+quadrant_label_font = 11
+
+breaks <- c(0,100,200,400,600)
+labels <- as.character(breaks)
+breaks <- log2(breaks + 100)
+
+threshold_val <- 100
+threshold <- log2(threshold_val + 100)
+
+ggplot(normLRT,aes(X,Y)) +
+  annotate("rect", xmin=threshold, xmax=Inf, ymin=threshold, ymax=Inf, size=1,color="grey50",fill=NA) +
+  annotate("rect", xmin=threshold, xmax=Inf, ymin=breaks[1], ymax=threshold, fill=mypal[2],alpha=0.2) +
+  annotate("rect", xmin=breaks[1], xmax=threshold, ymin=threshold, ymax=Inf, fill=mypal[1],alpha=0.2) +
+  geom_abline(slope=1,intercept = 0,linetype="dotted",color="gray") +
+  geom_point(alpha=.7,color="darkblue",size=1.5) +
+  geom_text_repel(data = se_labels, 
+                  size=(point_label_font / ggplot2:::.pt),
+                  aes(X,Y,label = symbol),
+                  segment.colour="grey50",
+                  segment.size = 0.25,
+                  force = 20,
+                  min.segment.length = 0) +
+  theme_bw(base_size=11) +
+  scale_x_continuous(breaks=breaks,labels=labels,limits=c(min_LRT,max_LRT)) +
+  scale_y_continuous(breaks=breaks,labels=labels,limits=c(min_LRT,max_LRT)) +
+  xlab("CRISPR LRT Score") +
+  ylab("RNAi LRT Score") +
+  scale_colour_continuous(name = "Number of\nDependent\nCell Lines\n(CRISPR)", labels = c("0", "300", "600"), breaks=c(0,300,600)) +
+  theme(legend.position="right") +
+  annotate("text", x = max_LRT, y = log2(50 + 100), label = paste0("CRISPR\n(",crispr_only_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=1) +
+  annotate("text", x = log2(25 + 100), y = log2(500+100), label = paste0("RNAi\n(",rnai_only_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=0) +
+  annotate("text", x = log2(500+100), y = log2(500+100), label = paste0("Shared\n(",both_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=.5)
+ggsave(file.path("figures","gene_deps_profiles_LRT_scatter.pdf"),width=3.5,height=2.5)
+
+       
---
+++ src/gene_dep_profiles/variance_scatter.R
@@ -0,0 +1,57 @@
+
+source("src/packages_paths.R")
+
+t1 <- fread(file.path("tables","Supplemental-Table-1.csv"))
+t1$entrez_id %<>% as.character(.)
+t1 %<>% subset(.,high_confidence)
+
+t2 <- fread(file.path("tables","Supplemental-Table-2.csv"))
+t2$entrez_id %<>% as.character(.)
+t2 %<>% subset(.,entrez_id %in% t1$entrez_id)
+
+crispr_var_thresh <- min(subset(t2,CRISPR_HVD)$CRISPR_dependency_variance)
+rnai_var_thresh <- min(subset(t2,RNAi_HVD)$RNAi_dependency_variance)
+
+var_df <- dplyr::select(t2,entrez_id,symbol,CRISPR_HVD,RNAi_HVD,CRISPR_dependency_variance,RNAi_dependency_variance)
+
+var_df$color_group <- "grey50" 
+var_df$color_group[var_df$CRISPR_HVD | var_df$RNAi_HVD] <- "darkblue"
+
+max_var <- max(c(var_df$CRISPR_dependency_variance,var_df$RNAi_dependency_variance))
+
+point_labels <- subset(var_df,(CRISPR_dependency_variance > .12) | (RNAi_dependency_variance > .12) | ((CRISPR_dependency_variance > .09) & (RNAi_dependency_variance > .09)))
+point_label_font <- 6
+
+crispr_only <- subset(var_df,CRISPR_HVD & !RNAi_HVD)
+crispr_only_count <- nrow(crispr_only)
+rnai_only <- subset(var_df,!CRISPR_HVD & RNAi_HVD)
+rnai_only_count <- nrow(rnai_only)
+both <- subset(var_df,CRISPR_HVD & RNAi_HVD)
+both_count <- nrow(both)
+
+point_pal <- c("grey50"="grey50","darkblue"="darkblue")
+mypal <- c("#A44E78","#4186C4","#E49042")
+ggplot(var_df,aes(x=CRISPR_dependency_variance,y=RNAi_dependency_variance)) +
+  annotate("rect", xmin=crispr_var_thresh, xmax=Inf, ymin=rnai_var_thresh, ymax=Inf, size=1,color="black",fill=NA) +
+  annotate("rect", xmin=crispr_var_thresh, xmax=Inf, ymin=0, ymax=rnai_var_thresh, fill=mypal[2],alpha=0.2) +
+  annotate("rect", xmin=0, xmax=crispr_var_thresh, ymin=rnai_var_thresh, ymax=Inf, fill=mypal[1],alpha=0.2) +
+  geom_abline(slope=1,intercept = 0,linetype="dotted",color="gray") +
+  geom_point(aes(color=color_group),alpha=.7,size=1.5) +
+  geom_text_repel(data = point_labels,
+                  size=(point_label_font / ggplot2:::.pt),
+                  aes(CRISPR_dependency_variance,RNAi_dependency_variance,label = symbol),
+                  segment.colour="grey50",
+                  segment.size = 0.25,
+                  force = 20,
+                  min.segment.length = 0) +
+  theme_bw(base_size=11) +
+  xlim(c(0,max_var)) +
+  ylim(c(0,max_var)) +
+  xlab("CRISPR Variance") +
+  ylab("RNAi Variance") +
+  scale_colour_manual(values=point_pal) +
+  theme(legend.position="none") +
+  annotate("text", x = .15, y = .025, label = paste0("CRISPR\n(",crispr_only_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=.5) +
+  annotate("text", x = .02, y = .15, label = paste0("RNAi\n(",rnai_only_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=0) +
+  annotate("text", x = .15, y = .15, label = paste0("Shared\n(",both_count,")"),size=(quadrant_label_font / ggplot2:::.pt),hjust=.5)
+ggsave(file.path("figures","gene_deps_profiles_variance_scatter.pdf"),width=3.5,height=2.5)
