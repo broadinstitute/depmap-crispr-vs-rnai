32d6c87265da288bedabe94bbdc00d5d6a234cf8
Ashir Borah <ashiraseesh@gmail.com>
2022-01-28 07:04:24 -0500

    Add a command to remove the zipfile at the end of the figshare downloader

--- src/figshare_downloader.R
+++ src/figshare_downloader.R
@@ -3,7 +3,9 @@ data_dir <- file.path("data","raw")
 zipfile <- file.path(data_dir,"collection.zip")
 
 options(timeout=200)
+dir.create(data_dir)
 download.file(url ="https://figshare.com/ndownloader/articles/16735132?private_link=24131e4e0b894826036e",
               destfile=zipfile)
 
 unzip(zipfile,exdir=data_dir)
+file.remove(zipfile)
