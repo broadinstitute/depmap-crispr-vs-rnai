fc7b69df710df24caebd195cd5e89234e767d27e
Ashir Borah <ashiraseesh@gmail.com>
2022-02-02 15:04:11 -0500

    Added a better data downloader

--- src/figshare_downloader.R
+++ src/figshare_downloader.R
@@ -1,11 +1,12 @@
 
+library(httr)
+
 data_dir <- file.path("data","raw")
 zipfile <- file.path(data_dir,"collection.zip")
-
-options(timeout=200)
 dir.create(data_dir)
-download.file(url ="https://figshare.com/ndownloader/articles/16735132?private_link=24131e4e0b894826036e",
-              destfile=zipfile)
+
+url <- "https://figshare.com/ndownloader/articles/16735132?private_link=24131e4e0b894826036e"
+GET(url, progress(), write_disk(zipfile, overwrite=TRUE))
 
 unzip(zipfile,exdir=data_dir)
 file.remove(zipfile)
