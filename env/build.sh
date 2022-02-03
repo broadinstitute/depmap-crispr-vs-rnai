3abbbeade06ab46dab61271978b8ab5603d985fd
Ashir Borah <ashiraseesh@gmail.com>
2022-02-02 19:48:24 -0500

    Changed path to build dockerfile

--- env/build.sh
+++ env/build.sh
@@ -1,4 +1,4 @@
 set -ev
 docker_image=us.gcr.io/cds-docker-containers/depmap-crispr-vs-rnai:1
-docker build -t ${docker_image} .
+docker build -f env/Dockerfile -t ${docker_image} .
 docker push ${docker_image}
