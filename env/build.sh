1472bc91b55fb39247977f9ef9ee39e0a3283818
J. Michael Krill-Burger <30512586+jmkrillburger@users.noreply.github.com>
2022-02-09 21:00:19 -0500

    Docker build script

--- env/build.sh
+++ env/build.sh
@@ -1,4 +1,4 @@
 set -ev
-docker_image=us.gcr.io/cds-docker-containers/depmap-crispr-vs-rnai:1
+docker_image=jkrillbu/depmap-crispr-vs-rnai:1
 docker build -t ${docker_image} .
 docker push ${docker_image}
